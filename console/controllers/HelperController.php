addfd00ec760734720bc928bb3c335db2d796f9e
shamsiddin <sh.muhibillayev@gmail.com>
2020-05-07 20:55:07 +0500

    match users fixed

--- console/controllers/HelperController.php
+++ console/controllers/HelperController.php
@@ -6,6 +6,7 @@ namespace console\controllers;
 
 use common\botHelpers\CommonHelper;
 use common\components\TelegramBot;
+use Longman\TelegramBot\Exception\TelegramException;
 use Yii;
 use yii\console\Controller;
 
@@ -13,9 +14,17 @@ class HelperController extends Controller
 {
     public function actionMatchUsers()
     {
-        $params = Yii::$app->params['bot'];
-        $telegram = new TelegramBot($params['bot_api_key'], $params['bot_username']);
-        $count = CommonHelper::matchUsers();
+        try {
+            $params = Yii::$app->params['bot'];
+            $telegram = new TelegramBot($params['bot_api_key'], $params['bot_username']);
+            $count = CommonHelper::matchUsers();
+        } catch (TelegramException $e) {
+            set_error_handler('var_dump', 0); // Never called because of empty mask.
+            @trigger_error("");
+            restore_error_handler();
+            // log telegram errors
+            echo $e->getMessage();
+        }
         return 'Users matched: ' . $count;
     }
 }
